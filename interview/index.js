3efaf30da0f883a44f10fd234b8501726c702655
badrudeen20 <badruddinansary20@gmail.com>
2024-03-03 15:44:25 +0530

    map,filter,reducer

---
+++ interview/index.js
@@ -0,0 +1,20 @@
+//MAP,FILTER,REDUCER
+
+/* const arr = [1,2,3]
+const mapArr = arr.map((e,i,a)=>{
+      return e * 2
+})
+const filterArr = arr.filter((e,i,a)=>{
+       return e > 1
+})
+
+const sum = arr.reduce((v,e,i,a)=>{
+   return v+e
+},0)
+const obj = arr.reduce((v,e,i,a)=>{
+   return {...v,[i]:e}
+},{})
+console.log(obj) */
+
+
+
